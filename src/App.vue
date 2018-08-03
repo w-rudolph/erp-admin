00d8be872e3d7b42de4b169e48ca0c2404d1ec0c
e <172247097@qq.com>
2018-08-03 14:28:28 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,22 @@
     <Aside />
     <Header />
     <div class="page-main">
-      <router-view/>
+      <Tabbar />
+      <div class="main-view">
+        <router-view/>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import Aside from "./views/common/Aside.vue";
-import Header from "./views/common/Header.vue";
+import { Component, Vue } from 'vue-property-decorator';
+import Aside from './views/common/Aside.vue';
+import Header from './views/common/Header.vue';
+import Tabbar from './views/common/Tabbar.vue';
 
 @Component({
-  components: { Aside, Header }
+  components: { Aside, Header, Tabbar }
 })
 export default class App extends Vue {}
 </script>
---
+++ src/views/common/Tabbar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="tab-bar">
+        <div class="tags">
+            <template v-for="i in 20">
+                <el-tag @click.native="onTagClick" :key="i" closable>标签{{i}}</el-tag>
+            </template>
+        </div>
+        <div class="settings">
+            <el-dropdown size="mini" split-button type="primary">
+                标签选项
+                <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>关闭所有</el-dropdown-item>
+                    <el-dropdown-item>关闭其它</el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+@Component
+export default class Tabbar extends Vue {
+  onTagClick(evt: any) {
+    evt.srcElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  }
+}
+</script>
+<style scoped>
+.tags {
+  line-height: 30px;
+  white-space: nowrap;
+  overflow: hidden;
+}
+.settings {
+  position: absolute;
+  right: 0;
+  top: 0;
+  box-shadow: -3px 0 6px 3px rgba(0, 0, 0, 0.1);
+}
+.el-tag {
+  margin: 0 2px;
+  cursor: pointer;
+}
+</style>
